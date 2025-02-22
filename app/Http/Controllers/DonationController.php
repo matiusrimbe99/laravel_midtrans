ca952072825c12ff87870fbdccf827b4c17c0e06
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2025-02-22 21:02:07 +0800

    Membuat fungsi store donation

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -2,12 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Donation;
 use Illuminate\Http\Request;
 
 class DonationController extends Controller
 {
+
     public function index()
     {
         return view('donation');
+    }
+
+    public function store(Request $request)
+    {
+        \DB::transaction(function () use ($request) {
+            $donation = Donation::create([
+                'donor_name' => $request->donor_name,
+                'donor_email' => $request->donor_email,
+                'donation_type' => $request->donation_type,
+                'amount' => floatval($request->amount),
+                'note' => $request->note,
+            ]);
+
+            $payload = [
+                'transaction_details' => [
+                    'order_id'      => 'SANDBOX-' . uniqid(),
+                    'gross_amount'  => $donation->amount,
+                ],
+                'customer_details' => [
+                    'first_name'    => $donation->donor_name,
+                    'email'         => $donation->donor_email,
+                    // 'phone'         => '08888888888',
+                    // 'address'       => '',
+                ],
+                'item_details' => [
+                    [
+                        'id'       => $donation->donation_type,
+                        'price'    => $donation->amount,
+                        'quantity' => 1,
+                        'name'     => ucwords(str_replace('_', ' ', $donation->donation_type))
+                    ]
+                ]
+            ];
+            $snapToken = \Midtrans\Snap::getSnapToken($payload);
+            $donation->snap_token = $snapToken;
+            $donation->save();
+
+            $this->response['snap_token'] = $snapToken;
+        });
+
+        return response()->json($this->response);
     }
 }
